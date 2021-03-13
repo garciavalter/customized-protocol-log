af364da5fa4c42c9693b0cc078a127b087c8aa8e
Valter Garcia <junior.opto@gmail.com>
2021-03-13 14:41:49 -0300

    login, logout and google login routes created.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const authRoutes = require('./routes/auth-routes');
+
 const app = express();
+
 const port = 3000;
 
 //set up view engine
 
 app.set('view engine', 'ejs');
+
+// set up routes
+
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.render('home');
