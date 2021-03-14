3b656c8d23de24ab98a135bf63be2b80cfc82230
Valter Garcia <junior.opto@gmail.com>
2021-03-14 08:41:51 -0300

    improved the links function for better maintence

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,10 +1,22 @@
 const form = document.querySelector('form');
 
+const links = {
+    google: 'auth/google',
+    login: 'auth/login'
+}
+
 console.log(form)
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    if(e.submitter.value){
+    console.log(e);
+    window.open(links[e.submitter.value], "_self");
+  
+/*    if(e.submitter.value === 'Google'){
         window.open("auth/google", "_self");
     }
+    else if (e.submitter.value === 'Log In'){
+        console.log('you clicked on log in');
+    }
+*/
 });
